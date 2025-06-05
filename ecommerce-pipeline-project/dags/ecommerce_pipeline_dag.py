a74bba5165664b6731944b30bd780cd2b6bd1e5c
meriem amrane <amrane.meriem07@gmail.com>
2025-06-05 17:10:05 +0100

    Update comments to be more descriptive and in English

--- ecommerce-pipeline-project/dags/ecommerce_pipeline_dag.py
+++ ecommerce-pipeline-project/dags/ecommerce_pipeline_dag.py
@@ -5,38 +5,44 @@ from airflow.utils.dates import days_ago
 import sys
 import os
 
-# Add the project root directory to Python path
+# Add the project root directory to Python path to allow importing local modules
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+# Import the main function from our extract script
 from extract.extract import main as extract_main
 
-# Define default arguments
+# Define default arguments for the DAG
+# These arguments will be applied to all tasks in the DAG
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'owner': 'airflow',              # Owner of the DAG
+    'depends_on_past': False,        # Don't wait for previous runs to complete
+    'email_on_failure': False,       # Don't send emails on failure
+    'email_on_retry': False,         # Don't send emails on retry
+    'retries': 1,                    # Number of retries if task fails
+    'retry_delay': timedelta(minutes=5),  # Wait 5 minutes between retries
 }
 
-# Create DAG
+# Create the DAG object
+# This defines the workflow and its schedule
 dag = DAG(
-    'ecommerce_pipeline',
-    default_args=default_args,
-    description='E-commerce data pipeline for processing CSV files',
-    schedule_interval=timedelta(days=1),
-    start_date=days_ago(1),
-    catchup=False,
-    tags=['ecommerce', 'data-pipeline'],
+    'ecommerce_pipeline',            # Unique identifier for the DAG
+    default_args=default_args,       # Default arguments defined above
+    description='E-commerce data pipeline for processing CSV files',  # DAG description
+    schedule_interval=timedelta(days=1),  # Run daily
+    start_date=days_ago(1),          # Start from yesterday
+    catchup=False,                   # Don't run for past dates
+    tags=['ecommerce', 'data-pipeline'],  # Tags for filtering in the UI
 )
 
-# Define tasks
+# Define the extract task
+# This task will run our CSV processing script
 extract_task = PythonOperator(
-    task_id='extract_data',
-    python_callable=extract_main,
-    dag=dag,
+    task_id='extract_data',          # Unique identifier for the task
+    python_callable=extract_main,    # Function to execute
+    dag=dag,                         # DAG this task belongs to
 )
 
-# Set task dependencies
+# Task dependencies
+# Currently, we only have one task, but we can add more tasks
+# and define their dependencies here
 extract_task  # This is our only task for now, but we can add more tasks later 
